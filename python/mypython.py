c4d4dc684b650997810ccf4b89b4c9f0765ead37
Alexander Nead-Work <neadwora@oregonstate.edu>
2019-01-28 04:07:13 -0800

    whoops, *now* it's actualy finished

--- python/mypython.py
+++ python/mypython.py
@@ -1,25 +1,69 @@
-#!/usr/bin/env python36
+#!/usr/bin/env python3
 
 # program: Program Py
 # author: Alexander Nead-Work
-# class/prof: cs344, w2019, Justin Goins
+# class/prof: CS344, W2019, Justin Goins
 
 # usage: ./mypython.py
 # (`python3 ./mypython.py` also works, but is not recommended)
 
 # resources used:
 # - https://stackoverflow.com/a/2257449 (random string generation)
+# - https://docs.python.org/3.5/library/random.html (random int)
+# - https://stackoverflow.com/a/13840441 (multiplying list contents)
 
+
+from functools import reduce
 import os, random, string, sys
 
-def get_random_str(n: int) -> str:
+
+def get_random_str(n):
 	"""Generate a random string of length n"""
 	return ''.join(random.choice(string.ascii_lowercase) for _ in range(n))
 
+
+def make_files(n, p, q):
+	"""Make n files with filenames of length p, with random contents of length q"""
+	files = []
+	for i in range(n):
+		files.append(get_random_str(p))
+		with open(files[i], "w") as f:
+			f.write("{}\n".format(get_random_str(q)))
+	return files
+
+
+def print_files(f):
+	"""Prints the contents of each file in f"""
+	for file in f:
+		with open(file, "r") as f:
+			print(''.join(f.readlines()), end='')
+
+
+def get_random_int(min, max):
+	"""Generate a random int in range [min, max]"""
+	return random.randint(min, max)
+
+
+def print_rands(n, min, max):
+	"""Generates n random ints in range [min, max], prints them, and prints the product"""
+	rands = []
+	for _ in range(n):
+		rands.append(get_random_int(min, max))
+
+	prod = reduce(lambda a, b: a*b, rands)
+
+	for r in rands:
+		print(r)
+
+	print(prod)
+
+
 def main():
-	for _ in range(3):
-		with open(get_random_str(10), "w") as f:
-			f.write("{}\n".format(get_random_str(10)))
+	files = make_files(3, 10, 10)
+
+	print_files(files)
+
+	print_rands(2, 1, 42)
 
 	return 0
 
