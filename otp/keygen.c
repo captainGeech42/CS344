80374bd68fe943f7e8cf7bada4f70fa7428dcafc
Alexander Nead-Work <neadwora@oregonstate.edu>
2019-03-06 09:35:43 -0800

    keygen

---
+++ otp/keygen.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+    // validate arguments
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s [key length]\n", argv[0]);
+        return 1;
+    }
+
+    // make sure a valid length was specified
+    int keylen = atoi(argv[1]);
+    if (keylen == 0) {
+        fputs("invalid key length specified\n", stderr);
+        return 1;
+    }
+
+    // seed rng
+    srand(time(NULL));
+
+    // alloc memory for the key
+    char *key = malloc(sizeof(char) * keylen);
+
+    // generate key
+    int r;
+    for (int i = 0; i < keylen; i++) {
+        r = rand() % 27;
+        if (r == 26) {
+            // add a space
+            key[i] = ' ';
+        } else {
+            // add a letter
+            key[i] = r + 'A';
+        }
+    }
+
+    // print key
+    puts(key);
+
+    // cleanup
+    free(key);
+    return 0;
+}
